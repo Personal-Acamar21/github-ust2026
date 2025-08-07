e17ce48c808b36e01ec6ae7e833c93afaf6fdc7e
Aboubacar M Camara <157975662+Personal-Acamar21@users.noreply.github.com>
2025-08-07 18:38:17 -0500

    Update Events.tsx

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -40,6 +40,12 @@ const events = [
     title: 'UST FC Spring Intramural Program',
     image: 'https://storage.googleapis.com/msgsndr/AKZP7FbfcOPsLo93Ayuw/media/67bfa5a6981fdc5e14a6ec7a.png',
     path: '/programs/spring-intramural-program'
+  },
+  {
+    id: 'fall-intramural-program',
+    title: 'UST FC Fall Intramural Program',
+    image: 'https://storage.googleapis.com/msgsndr/AKZP7FbfcOPsLo93Ayuw/media/6894823febb67c02968da335.png',
+    path: '/programs/fall-intramural-program'
   }
 ];
 
